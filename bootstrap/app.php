66904cbc9f36007f11c4c9447a4aa7a0ff62c642
fahmiitmamul <itmamulfahmi@outlook.co.id>
2026-04-11 10:59:21 +0700

    fix: update middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'contoh' => \App\Http\Middleware\ContohMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
