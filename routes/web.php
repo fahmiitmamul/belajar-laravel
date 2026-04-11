f5a88c7dbeddab8c66242f5fbcccc393041954fb
fahmiitmamul <itmamulfahmi@outlook.co.id>
2026-04-11 10:03:44 +0700

    feat: views and routers

--- routes/web.php
+++ routes/web.php
@@ -152,3 +152,18 @@ Route::get('/abort/401', function (){
 Route::get('/abort/500', function (){
     abort(500);
 });
+
+Route::view('/template', 'template');
+
+Route::controller(\App\Http\Controllers\UserController::class)->group(function () {
+    Route::get('/login', 'login')->middleware([\App\Http\Middleware\OnlyGuestMiddleware::class]);
+    Route::post('/login', 'doLogin')->middleware([\App\Http\Middleware\OnlyGuestMiddleware::class]);
+    Route::post('/logout', 'doLogout')->middleware([\App\Http\Middleware\OnlyMemberMiddleware::class]);
+});
+
+Route::controller(\App\Http\Controllers\TodolistController::class)
+    ->middleware([\App\Http\Middleware\OnlyMemberMiddleware::class])->group(function () {
+        Route::get('/todolist', 'todoList');
+        Route::post('/todolist', 'addTodo');
+        Route::post('/todolist/{id}/delete', 'removeTodo');
+    });
