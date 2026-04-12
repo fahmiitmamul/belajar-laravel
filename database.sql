67c82b5459a1a369c6d1ba2297357e65471c603f
fahmiitmamul <itmamulfahmi@outlook.co.id>
2026-04-12 11:29:17 +0700

    feat: db

---
+++ database.sql
@@ -0,0 +1,8 @@
+use belajar_laravel;
+
+CREATE TABLE categories (
+    id VARCHAR(100) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
