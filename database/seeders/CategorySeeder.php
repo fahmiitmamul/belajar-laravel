eda413c421439a20efd065932d78209fea6d0738
fahmiitmamul <itmamulfahmi@outlook.co.id>
2026-04-16 16:36:16 +0700

    fix: category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,21 +15,25 @@ class CategorySeeder extends Seeder
         DB::table('categories')->insert([
             'id' => 'SMARTPHONE',
             'name' => 'Smartphone',
+            'description' => 'Smartphone',
             'created_at' => '2020-10-10 10:10:10',
         ]);
         DB::table('categories')->insert([
             'id' => 'FOOD',
             'name' => 'Food',
+            'description' => 'Food',
             'created_at' => '2020-10-10 10:10:10',
         ]);
         DB::table('categories')->insert([
             'id' => 'LAPTOP',
             'name' => 'Laptop',
+            'description' => 'Laptop',
             'created_at' => '2020-10-10 10:10:10',
         ]);
         DB::table('categories')->insert([
             'id' => 'FASHION',
             'name' => 'Fashion',
+            'description' => 'Fashion',
             'created_at' => '2020-10-10 10:10:10',
         ]);
     }
