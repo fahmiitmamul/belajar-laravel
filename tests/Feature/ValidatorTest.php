d1b85730797c9aee753977b8ab30760f68c4d181
fahmiitmamul <itmamulfahmi@outlook.co.id>
2026-04-19 10:16:23 +0700

    fix: test validator valid data

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -84,3 +84,27 @@ test('TestValidatorMultipleRules', function () {
     self::assertFalse($validator->passes());
     self::assertTrue($validator->fails());
 });
+
+test('TestValidatorValidData', function () {
+    $data = [
+        'username' => 'admin@pzn.com',
+        'password' => 'rahasia',
+    ];
+
+    $rules = [
+        'username' => 'required|email|max:100',
+        'password' => 'required|min:6|max:20',
+    ];
+
+    $validator = Validator::make($data, $rules);
+    self::assertNotNull($validator);
+
+    try {
+        $valid = $validator->validate();
+        Log::info(json_encode($valid, JSON_PRETTY_PRINT));
+    } catch (ValidationException $exception) {
+        self::assertNotNull($exception->validator);
+        $message = $exception->validator->errors();
+        Log::error($message->toJson(JSON_PRETTY_PRINT));
+    }
+});
