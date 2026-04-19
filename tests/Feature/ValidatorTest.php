88ab5bcbd846ae0dc6a6b1fc59c9bad1fe3864d0
fahmiitmamul <itmamulfahmi@outlook.co.id>
2026-04-19 09:34:37 +0700

    fix: update laravel validator

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -15,4 +15,25 @@ test('TestValidator', function () {
 
     $validator = Validator::make($data, $rules);
     self::assertNotNull($validator);
+
+    self::assertTrue($validator->passes());
+    self::assertFalse($validator->fails());
+});
+
+test('TestValidatorInvalid', function () {
+    $data = [
+        'username' => '',
+        'password' => '',
+    ];
+
+    $rules = [
+        'username' => 'required',
+        'password' => 'required',
+    ];
+
+    $validator = Validator::make($data, $rules);
+    self::assertNotNull($validator);
+
+    self::assertFalse($validator->passes());
+    self::assertTrue($validator->fails());
 });
