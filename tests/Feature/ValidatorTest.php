8439367e67d9aab91a2e3b520bb7001f32c275b8
fahmiitmamul <itmamulfahmi@outlook.co.id>
2026-04-19 09:30:12 +0700

    fix: update validator test

---
+++ tests/Feature/ValidatorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Validator;
+
+test('TestValidator', function () {
+    $data = [
+        "username" => "admin",
+        "password" => "12345"
+    ];
+
+    $rules = [
+        "username" => "required",
+        "password" => "required"
+    ];
+
+    $validator = Validator::make($data, $rules);
+    self::assertNotNull($validator);
+});
